83038dc986d148160352f25eb60946c844f1cbbf
Clément Aubin <aubincleme@gmail.com>
2021-07-30 10:36:56 +0200

    OIDC-114 : Allow to set-up multiple client configuration
    * Define a fallback for client configuration when no cookie is provided

--- oidc-authenticator/src/main/java/org/xwiki/contrib/oidc/auth/internal/configuration/ContextOIDCClientConfigurationProvider.java
+++ oidc-authenticator/src/main/java/org/xwiki/contrib/oidc/auth/internal/configuration/ContextOIDCClientConfigurationProvider.java
@@ -27,10 +27,22 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.configuration.ConfigurationSource;
 import org.xwiki.container.Container;
+import org.xwiki.contrib.oidc.internal.OIDCConfiguration;
 
 /**
  * Provide the {@link OIDCClientConfiguration} corresponding to the current context.
+ *
+ * The choice of the client configuration takes several configuration into account :
+ * <ul>
+ *     <li>The provider will first check if a cookie defined in CLIENT_CONFIGURATION_COOKIE_PROPERTY exists, forcing
+ *     the use of a given client configuration.</li>
+ *     <li>If no cookie is defined, or if a cookie is defined but no configuartion exist with this name, the provider
+ *     will fallback on the configuration defined in DEFAULT_CLIENT_CONFIGURATION_PROPERTY.</li>
+ *     <li>If the fallback configuration does not exist, the provider will fallback on the default configuration
+ *     available through the component manager.</li>
+ * </ul>
  *
  * @since 1.29
  * @version $Id$
@@ -40,9 +52,29 @@ import org.xwiki.container.Container;
 public class ContextOIDCClientConfigurationProvider implements Provider<OIDCClientConfiguration>
 {
     /**
-     * The name of the cookie in which the OIDC client configuration is defined.
+     * The name of the property in which the name of the OIDC configuration should be stored.
      */
-    public static final String OIDC_CONFIGURATION_REQUEST_PROPERTY = "oidcClientConfiguration";
+    public static final String CLIENT_CONFIGURATION_COOKIE_PROPERTY = OIDCConfiguration.PREFIX_PROP
+        + "clientConfigurationCookie";
+
+    /**
+     * The default name of the cookie in which the OIDC client configuration is defined.
+     */
+    public static final String DEFAULT_OIDC_CONFIGURATION_COOKIE = "oidcProvider";
+
+    /**
+     *
+     */
+    public static final String DEFAULT_CLIENT_CONFIGURATION_PROPERTY = OIDCConfiguration.PREFIX_PROP
+        + "defaultClientConfiguration";
+
+    /**
+     * Default client configuration to use when no configuration is defined.
+     */
+    public static final String DEFAULT_CLIENT_CONFIGURATION = "default";
+
+    @Inject
+    private ConfigurationSource configurationSource;
 
     @Inject
     private Container container;
@@ -53,16 +85,28 @@ public class ContextOIDCClientConfigurationProvider implements Provider<OIDCClie
     @Override
     public OIDCClientConfiguration get()
     {
+        String cookieName = configurationSource.getProperty(CLIENT_CONFIGURATION_COOKIE_PROPERTY,
+            DEFAULT_OIDC_CONFIGURATION_COOKIE);
+
+        String fallbackProviderName = configurationSource.getProperty(DEFAULT_CLIENT_CONFIGURATION_PROPERTY,
+            DEFAULT_CLIENT_CONFIGURATION);
+
         try {
+            // Check if a cookie exists, indicating which configuration to use
             if (container.getRequest() instanceof HttpServletRequest) {
                 HttpServletRequest request = (HttpServletRequest) container.getRequest();
 
                 for (Cookie cookie : request.getCookies()) {
-                    if (OIDC_CONFIGURATION_REQUEST_PROPERTY.equals(cookie.getName())
+                    if (cookieName.equals(cookie.getName())
                         && componentManager.hasComponent(OIDCClientConfiguration.class, cookie.getValue())) {
                         return componentManager.getInstance(OIDCClientConfiguration.class, cookie.getValue());
                     }
                 }
+            }
+
+            // Try to fallback on the configuration provided in the XWiki configuration
+            if (componentManager.hasComponent(OIDCClientConfiguration.class, fallbackProviderName)) {
+                return componentManager.getInstance(OIDCClientConfiguration.class, fallbackProviderName);
             }
 
             // Return the default config if nothing is found
